0659ec5768c5941cca0657e456d47af6d2fabb19
programare2019colegiu <programare2019colegiu@gmail.com>
2023-01-30 09:12:20 +0200

    a

---
+++ dist/src/index.js
@@ -0,0 +1,13 @@
+import * as argon2 from "argon2";
+import mongoose from "mongoose";
+try {
+    const hash = await argon2.hash("password", { hashLength: 200 });
+    console.log(hash);
+    const UserSchema = new mongoose.Schema({
+        username: { type: String, required: true, unique: true },
+        password: { type: String, required: true },
+    });
+}
+catch (err) {
+    console.log(err);
+}
