3a5a2eaa1d6b2ac6ac326b09a320d01805c3d4ce
ion22666 <ionmocanu2266@gmail.com>
2023-02-05 06:21:30 +0200

    a

--- src/index.ts
+++ src/index.ts
@@ -1,13 +1,23 @@
 import * as argon2 from "argon2";
 import mongoose from "mongoose";
-try {
-    const hash = await argon2.hash("password", { hashLength: 200 });
-    console.log(hash);
+
+(async () => {
+    // const hash = await argon2.hash("password", { hashLength: 200 });
+    // let is_lvalid = await argon2.verify(hash, "password");
+    // console.log(hash);
+    // console.log(is_lvalid);
+
+    mongoose.set("strictQuery", false);
+
+    await mongoose.connect(`mongodb+srv://${process.env.MONGODB_USERNAME}:${process.env.MONGODB_PASSWORD}@cluster0.wgurxzm.mongodb.net/?retryWrites=true&w=majority`);
+    console.log(mongoose.connection.readyState);
 
     const UserSchema = new mongoose.Schema({
-        username: { type: String, required: true, unique: true },
+        username: { type: { name: String }, required: true, unique: true },
         password: { type: String, required: true },
     });
-} catch (err) {
-    console.log(err);
-}
+
+    const User = mongoose.model<typeof UserSchema>("User", UserSchema);
+
+    new User({ username: "Giovanni" }).save();
+})();
